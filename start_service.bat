869680266d61aedaae546307029d1793aa910143
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-04 01:19:13 +0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -24,5 +24,5 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :--------------------------------------
-net stop phantomRobot
-net start phantomRobot
+net stop tg_bot
+net start tg_bot
