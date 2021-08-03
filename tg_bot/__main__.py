fde724354fedbb72ea98f56cf2e9813de92d7ab6
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-03 17:59:13 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -143,7 +143,8 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_photo(
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="➕ Add To Me Your Group ➕",
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[
+                                                                                   InlineKeyboardButton(text="➕ Add To Me Your Group ➕",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)],
                                                                                  [
                                                                                    InlineKeyboardButton(text="Channel",
