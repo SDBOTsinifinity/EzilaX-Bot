ed22f4d3133e60577be033b56a57664093ff7af6
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-03 14:35:03 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,6 @@ from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, CERT_PATH, POR
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from tg_bot.modules import ALL_MODULES
 from tg_bot import dispatcher
-from tg_bot.config import OWNER_USERNAME
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
